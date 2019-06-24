c097c7a0ec193573b9c91b43c31959d9f8885dd9
jun zhao <abc20899@163.com>
2019-06-24 15:07:12 +0800

    no message

---
+++ src/main/kotlin/cn/junechiu/learnkotlin/coroutines/hello.kt
@@ -0,0 +1,24 @@
+package cn.junechiu.learnkotlin.coroutines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+//suspend fun main() = coroutineScope {
+//    launch {
+//        delay(1000)
+//        println("Kotlin Coroutines World!")
+//    }
+//    println("Hello")
+//}
+
+fun main() {
+    GlobalScope.launch {
+        // 在后台启动一个新的协程并继续
+        delay(1000L) // 非阻塞的等待 1 秒钟（默认时间单位是毫秒）
+        println("World!") // 在延迟后打印输出
+    }
+    println("Hello,") // 协程已在等待时主线程还在继续
+    Thread.sleep(2000L) // 阻塞主线程 2 秒钟来保证 JVM 存活
+}
